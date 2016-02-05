bdcf5a6a5da217cf57cf67651af28d888e1b8730
Mesfin Bekele Mekonnen <mesbekmek@gmail.com>
2016-02-05 17:05:31 -0500

    Created Project

---
+++ WordCount/WordCount/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WordCount
+//
+//  Created by Mesfin Bekele Mekonnen on 2/5/16.
+//  Copyright © 2016 Mesfin Bekele Mekonnen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
